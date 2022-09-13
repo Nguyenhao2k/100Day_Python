c90174781062065f85a1736e10ad2c06a6e758a1
Nguyenhao2k <91106203+Nguyenhao2k@users.noreply.github.com>
2022-09-13 07:10:38 +0700

    update

--- Day13/main.py
+++ Day13/main.py
@@ -27,19 +27,17 @@
 # else:
 #     print("Mi deo du tuoi!!!")
 
-#Print is Your Friend
-
-pages = int(input("Number of pages: "))
-word_per_page = int(input("Number of words per page: "))
-total_words = pages * word_per_page
-print(total_words)
+# #Print is Your Friend
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
 
 # #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
-
-# mutate([1,2,3,5,8,13])
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+mutate([1,2,3,5,8,13])
