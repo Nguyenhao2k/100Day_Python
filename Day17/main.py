366ada997b9da2042f56683abe99393c8e15983e
Nguyenhao2k <91106203+Nguyenhao2k@users.noreply.github.com>
2022-09-23 23:09:53 +0700

    fix Day17

---
+++ Day17/main.py
@@ -0,0 +1,10 @@
+class Person:
+      def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+p2 = Person("Nguyen", 22)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
